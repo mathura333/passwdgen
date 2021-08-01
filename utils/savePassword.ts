dcd1690fb11cf4ede6a53267989c0bd0a0c2a359
mathura333 <mathuradas333@gmail.com>
2021-08-01 07:05:09 +0530

    Updated save password file

--- utils/savePassword.ts
+++ utils/savePassword.ts
@@ -7,7 +7,9 @@ const savePassword = (password: string, file = 'password.txt') => {
   fs.open(path.join(process.cwd(), file), 'a', 765, (e, id) => {
     fs.write(id, password + os.EOL, null, 'utf-8', () => {
       fs.close(id, () => {
-        console.log(chalk.green(`Password saved to ${file}`));
+        console.log(
+          chalk.green(`Password saved to ${path.join(process.cwd(), file)}`)
+        );
       });
     });
   });
